7d2cecac580c5570430ce14bf0b48789bc5c48c2
Robert Elwell <robert@wikia-inc.com>
2013-12-13 14:01:41 -0800

    voting route

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,6 @@ from app import views
 urlpatterns = patterns('',
                        url(r'active_campaigns/?', views.active_campaigns),
                        url(r'campaign/(\d+)/?', views.campaign_data),
-                       url(r'opponents/?', views.opponents)
+                       url(r'opponents/?', views.opponents),
+                       url(r'vote/(\d+)/(\d+)/?', views.vote),
 )
