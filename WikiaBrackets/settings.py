4789825d9b996a5462062cd3b88ccf053eca3002
Robert Elwell <robert@wikia-inc.com>
2013-12-13 14:43:10 -0800

    allow cross-domain xhr

--- WikiaBrackets/settings.py
+++ WikiaBrackets/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'app.middleware.crossdomainxhr.XsSharing'
 )
 
 ROOT_URLCONF = 'WikiaBrackets.urls'
